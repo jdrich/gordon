5038dc13d79aa44ab52460b0af7d912001050efb
root <root@jonathanri.ch>
2015-01-27 22:16:48 -0500

    Adding index page.

--- src/view/_head.php
+++ src/view/_head.php
@@ -1,7 +1,6 @@
 <head>
   <title><?php echo $title; ?> | Flash Gordon</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
   <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
---
+++ src/view/index.php
@@ -0,0 +1,8 @@
+<div class="panel">
+  <a href="study" role="button" class="btn btn-info btn-lg btn-block">Study</a> 
+  <a href="test" role="button" class="btn btn-warning btn-lg btn-block">Test</a>
+  <hr />
+  <a href="add" role="button" class="btn btn-default btn-lg btn-block">Add Flash Card</a>
+  <a href="list" role="button" class="btn btn-default btn-lg btn-block">Browse Flash Cards</a>
+</div>
+
